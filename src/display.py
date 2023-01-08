92a68f6cf3baff9edb7732e686b4a2dac9882c85
Mark <53943861+mdemore2@users.noreply.github.com>
2023-01-08 17:09:37 -0500

    added display code, flight tinkering + base imgs

---
+++ src/display.py
@@ -0,0 +1,50 @@
+from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
+from PIL import Image
+from datetime import datetime, timedelta
+import time
+
+
+class Display:
+    def __init__(self):
+        self._options = RGBMatrixOptions()
+        self._options.rows = 32
+        self._options.cols = 64
+        self._options.chain_length = 1
+        self._options.parallel = 1
+        self._options.hardware_mapping = 'adafruit-hat'
+
+        self._matrix = RGBMatrix(options=self._options)
+
+        self._font = graphics.font()
+        self._font.LoadFont()
+        self._text_color = graphics.Color(255, 255, 0)
+
+        self._duration = timedelta(seconds=30)
+
+    def send_text(self, text: str):
+        offscreen_canvas = self._matrix.CreateFrameCanvas()
+        pos = offscreen_canvas.width
+        start = datetime.utcnow()
+
+        while (start + self._duration) < datetime.utcnow():
+            offscreen_canvas.Clear()
+            len = graphics.DrawText(offscreen_canvas, self._font, pos, 10, self._text_color, text)
+            pos -= 1
+            if (pos + len) < 0:
+                pos = offscreen_canvas.width
+
+            time.sleep(0.05)
+            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+
+    def send_image(self, path: str):
+        image = Image.open(path)
+        image.thumbnail((self._matrix.width, self._matrix.height), Image.ANTIALIAS)
+        self._matrix.SetImage(image.convert('RGB'))
+        start = datetime.utcnow()
+        while (start + self._duration) < datetime.utcnow():
+            time.sleep(5)
+        #TODO: wipe screen
+
+
+
+
--- src/flights.py
+++ src/flights.py
@@ -2,17 +2,15 @@ from FlightRadar24.api import FlightRadar24API
 from config import bounds_str
 
 
-class frAPI:
+class FRapi:
     def __init__(self):
         self.fr = FlightRadar24API()
         self.bounds = bounds_str
 
     def ret_flights(self):
         flights = self.fr.get_flights(bounds=self.bounds)
-        return flights
-
-    def get_flight_details(self, flights):
         for flight in flights:
             details = self.fr.get_flight_details(flight.id)
             flight.set_flight_details(details)
         return flights
+
